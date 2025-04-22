b03ffaf0bd7da3a9f7e210ac2f38bf8ce0e62f47
BhavanasiNeha06 <bhavanasineha06@gmail.com>
2025-04-22 20:50:32 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+let navbar = document.querySelector('.header .navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+document.querySelector('#nav-close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+
+//----------- Toggle Search Form Updated----------------//
+const searchBtn = document.getElementById('search-btn');
+const closeSearch = document.getElementById('close-search');
+const searchForm = document.querySelector('.search-form');
+
+searchBtn.addEventListener('click', () => {
+    searchForm.classList.add('active');
+});
+
+closeSearch.addEventListener('click', () => {
+    searchForm.classList.remove('active');
+});
+
+window.onscroll=()=>{
+    navbar.classList.remove('active');
+    if(window.scrollY>0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+window.onload=()=>{
+    if(window.scrollY>0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
